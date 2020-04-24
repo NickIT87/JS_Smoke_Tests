bf04ae6103cb9ef449f41a67ecf10576d3b6df7b
Nickolazz1 <elanir358@gmail.com>
2020-04-24 14:02:53 +0300

    base routes overview

--- backend_tutorial/index.js
+++ backend_tutorial/index.js
@@ -3,7 +3,16 @@ let express = require('express');
 let app = express();
 
 app.get('/', function(req, res) {
-    res.send('Hello world');
+    res.send('This is home');
 });
+
+app.get('/news', function(req, res) {
+    res.send('This is news');
+});
+
+app.get('/news/:id', function(req, res) {
+    res.send('ID is -' + req.params.id);
+});
+
 
 app.listen(3000);
