62de3030aece9585d0a26c765aa10b8101659a0f
Nickolazz1 <elanir358@gmail.com>
2020-12-18 21:15:39 +0200

    think about types

--- typescript/test.ts
+++ typescript/test.ts
@@ -1,3 +1,10 @@
 const msg: string = 'hello worldos'
 let a: number = 10.2354
 console.log(msg, a)
+const contact: [string, number] = ['test', 12343214]
+let test: any = 42
+test = "dsafgdsdfgd"
+
+function sayMyName(name: string): void {
+    console.log(name)
+}
