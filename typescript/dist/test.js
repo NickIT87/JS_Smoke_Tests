a337085117a3bc916c79d827e1d328a989ce506e
Nickolazz1 <elanir358@gmail.com>
2021-05-06 19:08:08 +0300

    webdriverio

---
+++ typescript/dist/test.js
@@ -0,0 +1,10 @@
+"use strict";
+var msg = 'hello worldos';
+var a = 10.2354;
+console.log(msg, a);
+var contact = ['test', 12343214];
+var test = 42;
+test = "dsafgdsdfgd";
+function sayMyName(name) {
+    console.log(name);
+}
