fdbba439958a8ed8864f79c00ed00e02898812ae
Nickolazz1 <elanir358@gmail.com>
2021-06-27 18:40:27 +0300

    express js

--- Express_JS/app.js
+++ Express_JS/app.js
@@ -1,11 +1,17 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.sendFile(path.resolve(__dirname, 'index.html'))
+})
+
+app.all('*', (req, res) => {
+    res.status(404).send('<h1>Error 404, resource not found!</h1>')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
